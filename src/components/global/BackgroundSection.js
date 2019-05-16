ef276ddd0c141330eb0fd6b24b861082604c49f9
Sungmiii <christyksmile@gmail.com>
2019-05-16 13:38:19 +1200

    set up gatsbt-background-image and grapql

---
+++ src/components/global/BackgroundSection.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import BackgroundImage from 'gatsby-background-image';
+
+
+const BackgroundSection = ({
+    img,
+    styleClass,
+    title,
+    children
+}) => {
+    return (
+        <BackgroundImage
+            className={styleClass}
+            fluid={img}
+        >
+            <h1 className="title text-white text-uppercase text-center dispaly-4 font-weight-bold">
+                {title}
+            </h1>
+            {children}
+        </BackgroundImage>
+    );
+}
+
+export default BackgroundSection;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,24 @@
-import React from "react"
+import React from "react";
+import { graphql } from 'gatsby';
 
+import SEO from "../components/seo";
+import Layout from '../components/layout';
 
-import SEO from "../components/seo"
-import Layout from '../components/layout'
-
-export default () => (
+export default ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Welcome to Crown Coffee</h1>
   </Layout>
 );
+
+export const query = graphql`
+{
+  ima:file(relativePath:{eq:"cafehero.jpeg"}){
+    childImageSharp{
+      fluid{
+        ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+}
+`
