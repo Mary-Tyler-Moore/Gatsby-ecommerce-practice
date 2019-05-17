150cf4d411d2ea0a139522c7c8b8778bac72d25f
Sungmiii <christyksmile@gmail.com>
2019-05-17 16:33:13 +1200

    hook system getting error so.. back to class component

--- src/components/homepage/Menu.js
+++ src/components/homepage/Menu.js
@@ -2,14 +2,47 @@ import React, { useState, useEffect } from 'react';
 import Title from '../global/Title';
 import Image from 'gatsby-image';
 
-
-const Menu = () => {
-
-    return (
-        <div>
-            Content from Menu component
-        </div>
-    )
+class Menu extends React.Component {
+    state = {
+        items: this.props.items.edges,
+        coffeeItems: this.props.items.edges
+    }
+    renderItems = () => {
+        if (this.state.items.length > 0) {
+            return (
+                <section className="menu py-5">
+                    <div className="container">
+                        <Title title="Our Menu" />
+                        <div className="row mb-5">
+                            <div className="col">
+                                <h1>These are our itmes</h1>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            )
+        } else {
+            return (
+                <section className="menu py-5">
+                    <div className="container">
+                        <Title title="Our Menu" />
+                        <div className="row">
+                            <div className="col-10 col-sm-6 mx-auto text-center text-capitalize">
+                                <h1>There are no items to display</h1>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            )
+        }
+    }
+    render() {
+        return (
+            <div>
+                {this.renderItems()}
+            </div>
+        )
+    }
 }
 
 export default Menu;
